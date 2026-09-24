8f9eb39a164d9cc73f6159d0d54999e7b1a0a12c
jhota08 <batistajuliaoj@gmail.com>
2026-09-24 19:37:20 -0400

    feat: servidor Express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) =>{
+     res.send('API de Tarefas no ar')
+});
+
+app.listen(3000, () =>{
+    console.log('Servidor rodando na porta 3000')
+    
+});
+
